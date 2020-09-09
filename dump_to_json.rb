972164487fbc0ec1dd7a4c2c1f4edf0b5672d987
abetomo <abe@enzou.tokyo>
2020-09-09 10:45:33 +0900

    Add a script to output JSONL from dump file in Ruby

---
+++ dump_to_json.rb
@@ -0,0 +1,39 @@
+require 'csv'
+require 'json'
+
+File.open('./test.sql', 'r') do |f|
+  in_create_statement = false
+  columns = []
+  f.each_line do |line|
+    if line.index('CREATE') == 0
+      in_create_statement = true
+      next
+    end
+
+    if in_create_statement
+      start_quote_index = line.index('`')
+      if !start_quote_index || start_quote_index > 3
+        in_create_statement = false
+        next
+      end
+
+      end_quote_index = line.index('`', start_quote_index + 1)
+      columns.push(line[start_quote_index + 1 .. end_quote_index - 1])
+
+      if line.index(')') == 0
+        in_create_statement = false
+      end
+    end
+
+
+    if line.index('INSERT') == 0
+      line = line[line.index('(') + 1 .. line.length - 4]
+      line.split('),(').each do |values_csv|
+        # TODO numeric type
+        puts JSON.generate(Hash[columns.zip(CSV.parse(values_csv)[0])])
+      end
+
+      columns = []
+    end
+  end
+end
